08498255eb6d334756a4f2710a4e51e42fda2a24
Dan Kolz <dan@emergentideas.com>
2018-01-24 15:25:00 -0600

    made webhandle global, missing template logging, log term vrsc expires headers

--- app.js
+++ app.js
@@ -22,10 +22,18 @@ module.exports = function(projectRoot) {
         // set locals, only providing error in development
         res.locals.message = err.message;
         res.locals.error = req.app.get('env') === 'development' ? err : {};
+		
+		if(err.status == 404) {
+			// render the error page
+	        res.status(err.status);
+	        res.render('not-found');
+		}
+		else {
+			// render the error page
+	        res.status(err.status || 500);
+	        res.render('error');
+		}
 
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
     });
     return app
 }
