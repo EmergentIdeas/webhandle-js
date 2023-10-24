44bf65426d0362a54ac5db19f7b38bf7be7a017a
Dan Kolz <dan@emergentideas.com>
2023-10-24 15:38:18 -0500

    makes it possible to start from a module

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 let express = require('express');
-let path = require('path');
-const fs = require('fs')
 
 module.exports = function(projectRoot, callback) {
 
@@ -10,29 +8,12 @@ module.exports = function(projectRoot, callback) {
     wh.projectRoot = projectRoot
 
     wh.init(app, () => {
+		if(typeof require !== 'undefined' && require.main && require.main.require)
 		require.main.require('./server-js/add-routes')(wh.router)
 		if(callback) {
 			callback(null, wh)
 		}
 	})
 
-    // error handler
-    wh.routers.cleanup.use(function(err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-		
-		if(err.status == 404) {
-			// render the error page
-	        res.status(err.status);
-	        res.render('not-found');
-		}
-		else {
-			// render the error page
-	        res.status(err.status || 500);
-	        res.render('error');
-		}
-
-    });
     return app
 }
