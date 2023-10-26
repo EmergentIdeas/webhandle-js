215b979d8b668cd3d815335dee483466d966d842
Dan Kolz <dan@emergentideas.com>
2023-10-26 13:36:42 -0500

    fix bug when being started via pm2 via a module

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ module.exports = function(projectRoot, callback) {
     wh.projectRoot = projectRoot
 
     wh.init(app, () => {
-		if(typeof require !== 'undefined' && require.main && require.main.require)
-		require.main.require('./server-js/add-routes')(wh.router)
+		if(typeof require !== 'undefined' && require.main && require.main.require) {
+			try {
+				require.main.require('./server-js/add-routes')(wh.router)
+			}
+			catch(e) {
+				// probably not a big deal. This will happen if we've been invoked via a module
+			}
+		}
 		if(callback) {
 			callback(null, wh)
 		}
